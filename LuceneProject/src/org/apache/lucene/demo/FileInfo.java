32ffaa02d78beaee481a76e18b59bc539aba90a1
Steven Macleod <smacleod1993@gmail.com>
2015-10-16 16:42:32 +0100

    Extracting Article Information
    When you search a term, it will display the headline, leading paragraph
    etc of each result.

---
+++ LuceneProject/src/org/apache/lucene/demo/FileInfo.java
@@ -0,0 +1,63 @@
+package org.apache.lucene.demo;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+
+public class FileInfo {
+	private String filePath;
+	
+	public FileInfo(String filePath){
+		this.filePath = filePath;
+	}
+	
+	private String getAllText(String path){
+		try {
+			return new Scanner(new File(path)).useDelimiter("\\Z").next();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Error";
+		}
+	}
+	
+	public String getHeadline(){
+		Document doc = Jsoup.parse(getAllText(this.filePath));
+		Elements e=doc.select("HL"); 
+		return e.text();
+	}
+	
+	public String getDate(){
+		Document doc = Jsoup.parse(getAllText(this.filePath));
+		Elements e=doc.select("DATE"); 
+		return e.text();
+	}
+	
+	public String getLeadingParagraph(){
+		Document doc = Jsoup.parse(getAllText(this.filePath));
+		Elements e=doc.select("LP"); 
+		return e.text();
+	}
+	
+	public String getArticleText(){
+		Document doc = Jsoup.parse(getAllText(this.filePath));
+		Elements e=doc.select("TEXT"); 
+		return e.text();
+	}
+	
+	public String getDocumentId(){
+		Document doc = Jsoup.parse(getAllText(this.filePath));
+		Elements e=doc.select("DOCID"); 
+		return e.text();
+	}
+	
+	public String getDocumentNumber(){
+		Document doc = Jsoup.parse(getAllText(this.filePath));
+		Elements e=doc.select("DOCNO"); 
+		return e.text();
+	}
+}
